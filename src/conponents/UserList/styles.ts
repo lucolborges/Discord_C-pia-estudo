69bea047de9001576a156072ad6ca210caec00d4
lucolborges <lucolborges@hotmail.com>
2023-01-07 11:36:08 -0300

    UserList

--- src/conponents/UserList/styles.ts
+++ src/conponents/UserList/styles.ts
@@ -1,30 +1,93 @@
 import styled from "styled-components";
 
-import { ExpandMore } from '@styled-icons/material';
+import  { UserProps } from '.';
 
 export const Container = styled.div`
-  grid-area: SN;
+  grid-area: UL;
+  
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+
+  padding: 3px 6px 0 16px;
+  background-color: var(--secondary);
+  
+  max-height: calc(100vh - 46px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--tertiary);
+    border-radius: 4px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--secondary);
+  }
+`;
+
+export const Role = styled.span`
+  margin-top: 20px;
+
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  color: var(--gray);
+`;
+
+export const  User = styled.div`
+  margin-top: 5px;
+  padding: 5px;
+
+  display: flex;
   align-items: center;
 
-  padding: 0 11px 0 16px;
-  background: var(--secondary);
-
-  box-shadow: rgba(0,0,0,.2) 0 px 1px 0 0;
-  z-index: 2;
-  
-`;
-export const Title = styled.h1`
-  font-weight: bold;
-  font-size: 16px;
-  color: var(--white);
-`;
-
-export const ExpandIcon= styled(ExpandMore)`
-  width: 28px;
-  height: 28px;
-
-  color: var(--white);
   cursor: pointer;
+  border-radius: 4px;
+
+  background-color: transparent;
+
+  transition: background-color .2s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  > strong {
+    margin-left: 13px;
+    font-weight: 500;
+    color: var(--white);
+    opacity: 0.7;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  > span {
+    margin-left: 9px;
+    background-color: var(--discord);
+    color: var(--white);
+
+    border-radius: 4px;
+    padding: 4px 5px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 11px;
+  }
+`;
+
+export const  Avatar = styled.div`
+  flex-shrink: 0;
+  width: 32px;
+  height: 32px;
+
+  background-color: var(--primary);
+  background-color: ${userProps => userProps.isBot ? 'var()' : 'var()' };
+
+  border-radius: 50%;
+
+  
 `;
